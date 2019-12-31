0dbe50a81db4114f3891de98f69db4dd46aeda29
Bridget Asser <bridget.asser@gmail.com>
2019-12-31 15:47:24 -0500

    added partials for multipage layout

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 var items = ["Buy food", "Cook Food", "Eat Food"];
+var workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -19,13 +20,22 @@ app.get("/", function(req, res) {
   };
 
   var day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", { listTitle: "Work", newListItems: workItems });
 });
 
 app.listen(3000, function() {
